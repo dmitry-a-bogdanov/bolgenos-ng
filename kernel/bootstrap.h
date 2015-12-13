c2912975bb129d7e8154206dd0dd751c5920084a
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2015-12-13 13:51:24 +0300

    mm: detect available RAM
    * Added detection of available RAM size via multiboot.

--- kernel/bootstrap.h
+++ kernel/bootstrap.h
@@ -1,9 +1,11 @@
 #ifndef __BOLGENOS_NG__BOOTSTRAP_H__
-#define __BOLGENOS_NG__BOOTSTRAP_H__ (1)
+#define __BOLGENOS_NG__BOOTSTRAP_H__
 
-/*
- * KERNEL_STACK_SIZE defines size of kernel bootstrap stack in bytes
- */
+/**
+* \brief Kernel stack size.
+*
+* Macro defines size of kernel bootstrap stack in bytes.
+*/
 #if defined(CONFIG_KERNEL_STACK_SIZE)
 #	define KERNEL_STACK_SIZE (CONFIG_KERNEL_STACK_SIZE)
 #else
@@ -11,10 +13,11 @@
 #endif
 
 
-
 #if !defined(__ASSEMBLER__)
 
 #include <bolgenos-ng/int_types.h>
+#include <bolgenos-ng/mem_utils.h>
+
 
 #define __MBH_MAGIC (0x1badb002)
 
@@ -38,11 +41,47 @@ typedef struct __attribute__((packed)) {
 	uint32_t depth;
 } multiboot_header_t;
 
+
 #define mbh_initializer(flags_value) {				\
 		.magic = __MBH_MAGIC,				\
 		.flags = (flags_value),				\
 		.checksum = __mbh_checksum(flags_value)		\
 	}
+
+
+/**
+* \brief Multiboot info: memory.
+*
+* If this flag is set values mem_lower and mem_upper are valid.
+*/
+#define MBI_MEM_INFO			(1 << 0)
+
+
+typedef struct __attribute__((packed)) {
+	uint32_t flags;
+	uint32_t mem_lower; /*!< Amount of low memory in kilobytes. */
+	uint32_t mem_upper; /*!< Amount of high memory in kilobytes. */
+	uint32_t boot_device;
+	uint32_t cmdline;
+	uint32_t mods_count;
+	uint32_t mods_addr;
+	uint8_t syms[16];
+	uint32_t mmap_length;
+	uint32_t mmap_addr;
+	uint32_t drives_length;
+	uint32_t drives_addr;
+	uint32_t config_table;
+	uint32_t boot_loader_name;
+	uint32_t apm_table;
+	uint32_t vbe_control_info;
+	uint32_t vbe_mode_info;
+	uint16_t vbe_mode;
+	uint16_t vbe_interface_seg;
+	uint16_t vbe_interface_off;
+	uint16_t vbe_interface_len;
+} boot_info_t;
+check_type_size(boot_info_t, 88);
+
 
 #endif
 
--- kernel/main.c
+++ kernel/main.c
@@ -1,7 +1,9 @@
 #include <bolgenos-ng/asm.h>
 #include <bolgenos-ng/bolgenos-ng.h>
+#include <bolgenos-ng/error.h>
 #include <bolgenos-ng/irq.h>
 #include <bolgenos-ng/mmu.h>
+#include <bolgenos-ng/mem_utils.h>
 #include <bolgenos-ng/pic_8259.h>
 #include <bolgenos-ng/pic_common.h>
 #include <bolgenos-ng/printk.h>
@@ -17,12 +19,30 @@ multiboot_header_t
 	__attribute__((section(".multiboot_header"), used))
 	multiboot_header = mbh_initializer(MBH_ALIGN | MBH_MEMINFO);
 
+boot_info_t *__boot_loader_boot_info;
+
+boot_info_t boot_info;
+
+void extract_boot_info() {
+	memcpy(&boot_info, __boot_loader_boot_info, sizeof(boot_info_t));
+}
+
 void kernel_main() {
 	interrupts_disable();
+
+	extract_boot_info();
+
 	vga_console_init();
 	vga_clear_screen();
 
-	printk("Starting bolgenos-ng-" BOLGENOS_NG_VERSION "\n"); 
+	printk("Starting bolgenos-ng-" BOLGENOS_NG_VERSION "\n");
+	if (boot_info.flags & MBI_MEM_INFO) {
+		printk("Detected memory: low = %lu kb, high = %lu kb\n",
+			(long unsigned) boot_info.mem_lower,
+			(long unsigned) boot_info.mem_upper);
+	} else {
+		panic("Bootloader didn't provide memory info!\n");
+	}
 
 	setup_segments();
 	setup_interrupts();
