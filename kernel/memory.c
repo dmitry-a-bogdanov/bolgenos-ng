3174a2a01c9fb4b2f62fccab08d24681984abcf0
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2015-12-18 01:40:50 +0300

    mm: cleanup
    * Refactored of high memory initialization.
    * Added documentation.

--- kernel/memory.c
+++ kernel/memory.c
@@ -9,97 +9,204 @@
 
 /**
 * \brief Page size
-* TODO: doc
+*
+* Size of page in bytes.
 */
 #define PAGE_SIZE			(4 * 1024)
 
-/**
-* \brief kernel object addressed.
-* TODO: doc
-*/
-__link_from_asm__ char __kernel_obj_start[0];
-__link_from_asm__ char __kernel_obj_end[0];
 
 /**
-* \brief Aling specified address down.
-* TODO: doc
+* \brief Start of kernel object.
+*
+* Pointer to start of memory where kernel ELF is loaded.
+*
 */
-static char *align_addr_down(char *addr, size_t boundary) {
-	return (char *)(((size_t) addr) & ~(boundary - 1));
+__link_from_asm__ char __kernel_obj_start[0];
+
+
+/**
+* \brief End of kernel object.
+*
+* Pointer to end of memory where kernel ELF is loaded.
+*/
+__link_from_asm__ char __kernel_obj_end[0];
+
+
+/**
+* \brief Aling address down.
+*
+* Align given address down to specified boundary.
+*
+* \param addr Address to be aligned.
+* \param boundary Alignment boundary.
+* \return Aligned address.
+*/
+static void *align_addr_down(void *addr, size_t boundary) {
+	char *__addr = addr;
+	return (void *)(((size_t) __addr) & ~(boundary - 1));
 }
 
 
 /**
-* \brief Aling specified address up.
-* TODO: doc
+* \brief Aling specified address.
+*
+* Align given address up to specified boundary.
+*
+* \param addr Address to be aligned.
+* \param boundary Alignment boundary.
+* \return Aligned address.
 */
-static char *align_addr(char *addr, size_t boundary) {
-	return (((size_t) addr) & (boundary - 1)) ?
-		align_addr_down(addr, boundary) + boundary :
+static void *align_addr(void *addr, size_t boundary) {
+	char *__addr = addr;
+	return (((size_t) __addr) & (boundary - 1)) ?
+		align_addr_down(__addr, boundary) + boundary :
 		addr;
 }
 
-
-enum pp_free {
-	ppf_in_use		= 0x0,
-	ppf_free		= 0x1,
+/**
+* \brief Page descriptor structure.
+*
+* The structure is intended to describe page frame.
+*/
+struct __attribute__((packed)) page {
+	int free:1; /*!< Page is free flag. */
 };
 
-typedef struct __attribute__((packed)) {
-	enum pp_free		free:1;
-} pp_descr_t;
+
+/**
+* \brief Page is used flag.
+*
+* Flag for page::free field that the page is used.
+*/
+#define PAGE_USED			(0x0)
+
+
+/**
+* \brief Page is free flag.
+*
+* Flag for page::free field that the page is free.
+*/
+#define PAGE_FREE			(0x1)
+
 
 
 /**
 *
 * Used only for simplifying address arithmetics.
 */
-typedef struct __attribute__((packed)) {
+struct __attribute__((packed)) page_frame {
 	char __data[PAGE_SIZE];
-} pp_frame_t;
+};
+assert_type_size(struct page_frame, PAGE_SIZE);
 
-assert_type_size(pp_frame_t, PAGE_SIZE);
 
+/**
+* \brief Memory region descriptor.
+*
+* Structure describes memory regions like high and low memory.
+*/
 struct memory_region {
 	size_t size;
-	/*!< Size of region in PAGE_SIZE units. */
+	/*!< Number of allocatable pages in region. */
 
-	pp_descr_t *descriptors;
-	/*!< Pointer to array of pseudo-page descriptors. */
+	struct page *pages;
+	/*!< Pointer to array of page descriptors. */
 
-	pp_frame_t *pages;
-	/*!< Pointer to region of pseudo-pages. */
+	struct page_frame *frames;
+	/*!< Pointer to region of pages frames. */
 };
 
 
-static size_t __descr_pages(size_t pages_number) {
-	return (size_t) align_addr(
-		(char *) (sizeof(pp_descr_t) * pages_number),
-		PAGE_SIZE) / PAGE_SIZE;
+/**
+* \brief Calculate number of pages for page descriptors.
+*
+* The function calculates number of pages that is needed to keep page
+* descriptors for specified number of pages.
+*
+* \param pages Number of pages.
+* \return Number of pages for holding page descriptors.
+*/
+static size_t descriptor_pages(size_t pages) {
+	size_t required_memory = sizeof(struct page) * pages;
+	return (size_t) align_addr((void *) required_memory,
+			PAGE_SIZE) / PAGE_SIZE;
 }
 
-
-static size_t __pages_available(size_t free_pages) {
-	size_t pages = free_pages;
-	while ( __descr_pages(pages) + pages != free_pages
-			&& pages != 0)
-		--pages;
-	return pages;
-
-}
-
-
-static struct memory_region __system_memory;
-
-static void dump_system_memory() {
-	printk("[MEM_INFO] free memory: size = %lu, descriptors = %lu,"
-		" pages = %lu\n", __system_memory.size,
-		__system_memory.descriptors, __system_memory.pages);
-}
 
 /**
-* Memory map: [descripor pages][memory pages]
+* \brief Split page result.
+*
+* Structure for returning result of splitting memory region.
 */
+typedef struct {
+	size_t pages; /*!< Number of pages for page descriptors. */
+	size_t frames; /*!< Number of allocatable frames. */
+} mem_split_t;
+
+
+/**
+* \brief Split pages in memory region into descriptors and frames.
+*
+* Function splits pages from memory region into two sections: page descriptors
+* area and page frames area.
+*
+* \param free_pages Number of free pages in area.
+* \param split Pointer to \ref mem_split_t structure for keeping result.
+*/
+static void split_pages(size_t free_pages, mem_split_t *split) {
+	size_t pages = free_pages;
+	while ( descriptor_pages(pages) + pages != free_pages && pages != 0)
+		--pages;
+	split->frames = pages;
+	split->pages = free_pages - pages;
+}
+
+
+/**
+* \brief High memory.
+*
+* High memory region descriptor.
+*/
+static struct memory_region high_memory;
+
+
+/**
+* \brief Start of high memory.
+*
+* The address of the beginning of high memory.
+*/
+#define __high_memory_start		((char *) 0x100000)
+
+
+/**
+* \brief For each page loop.
+*
+* Loop through all page descriptor in memory region.
+*
+* \param m_region Pointer to memory region for looping.
+* \param name Name of iterator variable to be created.
+*/
+#define for_each_page(m_region, name)					\
+	for(struct page *name = (m_region)->pages;			\
+		name != (m_region)->pages + (m_region)->size;		\
+		++name)
+
+
+/**
+* \brief For each page loop starting from specified page.
+*
+* Loop through all page descriptor in memory region starting from specified
+* page.
+*
+* \param m_region Pointer to memory region for looping.
+* \param name Name of iterator variable to be created.
+* \param start Pointer to the first page descriptor for looping.
+*/
+#define for_each_page_from(m_region, name, start)			\
+	for(struct page *name = start;					\
+		name != (m_region)->pages + (m_region)->size;		\
+		++name)
+
 
 void init_memory() {
 	if (mboot_is_meminfo_valid()) {
@@ -110,72 +217,57 @@ void init_memory() {
 		panic("Bootloader didn't provide memory info!\n");
 	}
 
-	pp_frame_t *kpages_last_addr =
-		(pp_frame_t *) align_addr_down(__kernel_obj_end, PAGE_SIZE);
+	struct page_frame *highmem_first_free =
+		align_addr(__kernel_obj_end, PAGE_SIZE);
 
-	size_t kpages_last_page = ((size_t) kpages_last_addr) / PAGE_SIZE;
-	size_t free_page_first = kpages_last_page + 1;
+	struct page_frame *highmem_last_free = align_addr_down(
+		__high_memory_start + mboot_get_high_mem() * 1024,
+		PAGE_SIZE); // points to page that contains last RAM address.
 
-	printk("[MEM_INFO] last kpage = %lu\n",
-		(long unsigned) kpages_last_page);
+	printk("[MEM_INFO] highmem free frames: %lu...%lu\n",
+		(long unsigned) highmem_first_free,
+		(long unsigned) highmem_last_free);
 
-	size_t high_mem_last_addr = (size_t) align_addr_down(
-		(char *) (0x100000 + mboot_get_high_mem() * 1024),
-		PAGE_SIZE);
+	size_t highmem_free_pages = highmem_last_free - highmem_first_free;
+	printk("[MEM_INFO] highmem_free_pages=%lu\n", highmem_free_pages);
 
-	size_t high_mem_last_page = high_mem_last_addr/PAGE_SIZE;
-	size_t free_page_last = high_mem_last_page - 1;
+	mem_split_t highmem_split;
+	split_pages(highmem_free_pages, &highmem_split);
+	high_memory.size = highmem_split.frames;
+	high_memory.pages = (struct page *) highmem_first_free;
+	high_memory.frames = highmem_first_free + highmem_split.pages;
 
-	printk("[MEM_INFO] free pages: %lu...%lu\n",
-		(long unsigned) free_page_first,
-		(long unsigned) free_page_last);
-
-	__system_memory.size =
-		__pages_available(free_page_last - free_page_first + 1);
-	__system_memory.descriptors = (pp_descr_t *)
-		(free_page_first * PAGE_SIZE);
-	__system_memory.pages = ((pp_frame_t *) (free_page_first * PAGE_SIZE))
-		+ __descr_pages(__system_memory.size);
-
-	for(size_t p_i = 0; p_i != __system_memory.size; ++p_i) {
-		__system_memory.descriptors[p_i].free = ppf_free;
+	for_each_page(&high_memory, p) {
+		p->free = PAGE_FREE;
 	}
 
-	dump_system_memory();
+	printk("[MEM_INFO] high_memory: size=%lu, pages=%lu, frames=%lu\n",
+		(unsigned long) high_memory.size,
+		(unsigned long) high_memory.pages,
+		(unsigned long) high_memory.frames);
 }
 
-#define pp_descr_for_each(m_region, name)				\
-	for(pp_descr_t *name = (m_region)->descriptors;			\
-		name != (m_region)->descriptors + (m_region)->size;	\
-		++name)
-
-#define pp_descr_for_each_from(m_region, name, start)			\
-	for(pp_descr_t *name = start;					\
-		name != (m_region)->descriptors + (m_region)->size;	\
-		++name)
-
 void *alloc_pages(size_t n) {
-	pp_frame_t *mem = NULL;
-	pp_descr_for_each(&__system_memory, page) {
-		if (page->free == ppf_in_use) {
+	struct page_frame *mem = NULL;
+	for_each_page(&high_memory, page) {
+		if (page->free == PAGE_USED) {
 			continue;
 		}
 		size_t free_pages = 0;
-		pp_descr_for_each_from(&__system_memory, other_page, page) {
-			if (other_page->free == ppf_in_use)
+		for_each_page_from(&high_memory, other_page, page) {
+			if (other_page->free == PAGE_USED)
 				break;
 			++free_pages;
 			if (free_pages == n)
 				break;
 		}
 
-		size_t free_page_index = page - __system_memory.descriptors;
+		size_t free_page_index = page - high_memory.pages;
 
 		if (free_pages == n) {
-			mem = &__system_memory.pages[free_page_index];
+			mem = &high_memory.frames[free_page_index];
 			for (size_t i = 0; i != n; ++i) {
-				__system_memory.descriptors[free_page_index
-					+ i].free = ppf_in_use;
+				high_memory.pages[free_page_index + i].free = PAGE_USED;
 			}
 			break;
 		}
