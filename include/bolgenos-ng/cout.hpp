0e31d8d0b312d284bfe2bb05465f2bc3308fb683
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-02-27 22:56:17 +0300

    cio: add log level
    * added output stream implementation that supports log levels
    
    Activity: #27

--- include/bolgenos-ng/cout.hpp
+++ include/bolgenos-ng/cout.hpp
@@ -8,12 +8,13 @@ namespace cio {
 class OutStream {
 public:
 	using format_func_type = OutStream& (OutStream&);
+	using newline_callback_type = void (OutStream&);
 
-	OutStream() {
-	}
+	OutStream();
 
-	virtual ~OutStream() {
-	}
+	virtual ~OutStream();
+
+	virtual void set_newline_callback(newline_callback_type cb);
 
 	OutStream& operator<<(const char *string);
 	OutStream& operator<<(void *ptr);
@@ -24,11 +25,45 @@ public:
 	OutStream& operator<<(unsigned int val);
 
 	OutStream& operator<<(format_func_type formatter);
+
+protected:
+	void exec_newline_callback_if_needed();
+	newline_callback_type *newline_callback_ = nullptr;
+	bool run_newline_callback_ = true;
+
+	friend OutStream& endl(OutStream&);
+};
+
+
+class LogStream: public OutStream {
+public:
+	explicit LogStream(int log_level);
+	virtual ~LogStream();
+	int log_level() const;
+	void log_level(int);
+
+	template<typename T>
+	LogStream &operator<<(T val) {
+		if (log_level_ > system_log_level_) {
+			return *this;
+		}
+		OutStream::operator<<(val);
+		return *this;
+	}
+protected:
+	int log_level_;
+	static int system_log_level_;
 };
 
 
 OutStream& endl(OutStream &stream);
 
 extern OutStream cout;
+
+extern LogStream ccrit;
+extern LogStream cerr;
+extern LogStream cwarn;
+extern LogStream cinfo;
+extern LogStream cnotice;
 
 } // namespace cio
