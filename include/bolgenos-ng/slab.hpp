3d979d820953e0a5ace2e403be721ea617f152ef
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-03-13 10:54:14 +0300

    doc: changed comment style for slab header
    Changed doxygen comment from C-style to C++-style.
    
    Activity: -

--- include/bolgenos-ng/slab.hpp
+++ include/bolgenos-ng/slab.hpp
@@ -10,22 +10,19 @@ namespace memory {
 namespace allocators {
 
 
-/**
-* \brief Slab allocation area descriptor.
-*
-* The structure holds info for slab allocator.
-*/
+/// \brief Slab allocation area descriptor.
+///
+/// The structure holds info for slab allocator.
 class SlabAllocator {
 public:
-	/**
-	* \brief Initialize slab.
-	*
-	* The function allocates memory, setups allocation map and fills provided
-	* slab area descriptor.
-	*
-	* \param elem_size Size of element in slab area.
-	* \param nelems Number of elemnts in slab area.
-	*/
+
+	/// \brief Initialize slab.
+	///
+	/// The function allocates memory, setups allocation map and fills
+	/// provided slab area descriptor.
+	///
+	/// \param elem_size Size of element in slab area.
+	/// \param nelems Number of elemnts in slab area.
 	SlabAllocator(size_t elem_size, size_t nelems);
 
 
@@ -53,33 +50,27 @@ public:
 	bool initialize(size_t elem_size, size_t nelems);
 
 
-	/**
-	* \brief Allocate element from slab.
-	*
-	* The function allocates one element from slab if possible.
-	*
-	* \return Pointer to allocated memory or nullptr.
-	*/
+	/// \brief Allocate element from slab.
+	///
+	/// The function allocates one element from slab if possible.
+	///
+	/// \return Pointer to allocated memory or nullptr.
 	void *allocate();
 
 
-	/**
-	* \brief Free allocated element.
-	*
-	* The function frees specified element in the given slab area.
-	*
-	* \param addr Previously allocated element from this slab area.
-	*/
+	/// \brief Free allocated element.
+	///
+	/// The function frees specified element in the given slab area.
+	///
+	/// \param addr Previously allocated element from this slab area.
 	void deallocate(void *addr);
 
 
-	/**
-	* \brief Get initialization status of \ref SlabAllocator.
-	*
-	* The function returns initialization status of \ref SlabAllocator.
-	*
-	* \return true if ok, false if error has occured.
-	*/
+	/// \brief Get initialization status of \ref SlabAllocator.
+	///
+	/// The function returns initialization status of \ref SlabAllocator.
+	///
+	/// \return true if ok, false if error has occured.
 	bool is_initialized() const;
 
 
@@ -94,36 +85,33 @@ public:
 	bool owns(void *memory) const;
 
 
-	/**
-	* \brief \ref SlabAllocator destructor.
-	*
-	* The functions destructs \ref SlabAllocator object and frees memory
-	* related to this allocator.
-	*/
+	/// \brief \ref SlabAllocator destructor.
+	///
+	/// The functions destructs \ref SlabAllocator object and frees memory
+	/// related to this allocator.
 	~SlabAllocator();
 
+
 private:
-	/**
-	* \brief Get allocation status of memory unit.
-	*
-	* The function gets allocation status of specified memory unit
-	* in slab area.
-	* \param index Index of memory unit in slab.
-e	* \return true if unit is free, false otherwise.
-	*/
+
+	/// \brief Get allocation status of memory unit.
+	///
+	/// The function gets allocation status of specified memory unit
+	/// in slab area.
+	/// \param index Index of memory unit in slab.
+	/// \return true if unit is free, false otherwise.
 	bool is_free(size_t index);
 
 
-	/**
-	* \brief Set allocation status of memory unit.
-	*
-	* The function sets boolean value of allocation status
-	* to specified memory unit in the slab area.
-	* \param index Index of memory unit in slab.
-	* \param free Memory allocation status to be set. True if free,
-	* false otherwise.
-	*/
+	/// \brief Set allocation status of memory unit.
+	///
+	/// The function sets boolean value of allocation status
+	/// to specified memory unit in the slab area.
+	/// \param index Index of memory unit in slab.
+	/// \param free Memory allocation status to be set. True if free,
+	/// false otherwise.
 	void set_free(size_t index, bool free);
+
 
 	/// Size of element in size slab.
 	size_t elem_size_ = 0;
