1416aedebb45b209f68b5672c3579e90700b72a3
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-03-14 21:02:05 +0300

    memory: refactor
    * added constexpr specifier to are_consequent
    * added constexpr specifier to is_the_first_in_buddy
    * added constructors and descructors to mallocator
    
    Activity: #32

--- kernel/mallocator.hpp
+++ kernel/mallocator.hpp
@@ -13,15 +13,19 @@ namespace allocators {
 
 class Mallocator {
 public:
+	Mallocator() = default;
+	Mallocator(const Mallocator &) = delete;
+	Mallocator& operator =(const Mallocator &) = delete;
+	~Mallocator() = default;
 	void initialize(PageAllocator *fallback,
 			size_t chain_memory, size_t chain_length);
 	void *allocate(size_t bytes);
 	void deallocate(void *memory);
 private:
-	size_t chain_length_;
+	size_t chain_length_ = 0;
 	/// Chain of slab allocators.
-	SlabAllocator *chain_;
-	PageAllocator *fallback_;
+	SlabAllocator *chain_ = nullptr;
+	PageAllocator *fallback_ = nullptr;
 	SlabAllocator internal_allocator_;
 };
 
