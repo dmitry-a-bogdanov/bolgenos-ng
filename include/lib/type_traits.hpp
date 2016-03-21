bdfe4e49f99adfbaad9181d990656717663e42fe
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-03-21 19:36:37 +0300

    c++: extended type_traits
    * added stupid is_signed and is_unsigned and implementation
    * added enable_if
    * added make_unsigned
    
    Activity: #20

--- include/lib/type_traits.hpp
+++ include/lib/type_traits.hpp
@@ -30,4 +30,107 @@ struct integral_constant {
 };
 
 
+using true_type = lib::integral_constant<bool, true>;
+
+
+using false_type = lib::integral_constant<bool, false>;
+
+
+template<typename Type>
+struct is_unsigned: false_type {};
+
+
+template<>
+struct is_unsigned<unsigned long long>: true_type {};
+
+
+template<>
+struct is_unsigned<unsigned long>: true_type {};
+
+
+template<>
+struct is_unsigned<unsigned int>: true_type {};
+
+
+template<>
+struct is_unsigned<unsigned short>: true_type {};
+
+
+template<>
+struct is_unsigned<unsigned char>: true_type {};
+
+
+template<typename Type>
+struct is_signed: false_type {};
+
+
+template<>
+struct is_signed<long long>: true_type {};
+
+
+template<>
+struct is_signed<long>: true_type {};
+
+
+template<>
+struct is_signed<int>: true_type {};
+
+
+template<>
+struct is_signed<short>: true_type {};
+
+
+template<>
+struct is_signed<char>: true_type {};
+
+
+template<bool Cond, class T = void>
+struct enable_if {
+	// type is not declared in this class
+};
+
+
+template<class T>
+struct enable_if<true, T> {
+	using type = T;
+};
+
+
+template<typename T>
+struct make_unsigned {
+	using type = typename enable_if<is_unsigned<T>::value, T>::type;
+};
+
+
+template<>
+struct make_unsigned<char> {
+	using type = unsigned char;
+};
+
+
+template<>
+struct make_unsigned<short> {
+	using type = unsigned short;
+};
+
+
+template<>
+struct make_unsigned<int> {
+	using type = unsigned int;
+};
+
+
+template<>
+struct make_unsigned<long> {
+	using type = unsigned long;
+};
+
+
+template<>
+struct make_unsigned<long long> {
+	using type = unsigned long long;
+};
+
+
+
 } // namespace lib
