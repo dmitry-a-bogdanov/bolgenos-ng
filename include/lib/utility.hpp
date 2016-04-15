d979363b89cd48ac5f802b6edbee5becb7816325
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-04-15 23:29:27 +0300

    doc: document utility.hpp
    Added missing doc
    
    activity: #39

--- include/lib/utility.hpp
+++ include/lib/utility.hpp
@@ -4,10 +4,17 @@
 namespace lib {
 
 
+/// \brief Swap objects.
+///
+/// Exchange values of two objects.
+///
+/// \param a The first object, which content is swapped.
+/// \param b The second object, which content is swapped.
 template <class ValueType> void swap (ValueType& a, ValueType& b) {
 	ValueType tmp(a);
 	a = b;
 	b = tmp;
 }
+
 
 } // namespace lib
