1062d32d2dad452d1d85f6b3ea953c36d2c5d372
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-04-17 14:49:50 +0300

    execinfo: auto parsing stack frames
    * changed manual stack parsing to automatical
    
    activity: #20

--- kernel/backtrace.cpp
+++ kernel/backtrace.cpp
@@ -1,5 +1,6 @@
 #include <bolgenos-ng/execinfo.hpp>
 
+#include <bolgenos-ng/kernel_object.hpp>
 
 #include <lib/ostream.hpp>
 
@@ -12,12 +13,45 @@ struct  __attribute__((packed)) c_stack_frame {
 
 static_assert(sizeof(c_stack_frame) == 8, "Wrong size of structure\n");
 
+
+lib::ostream& operator <<(lib::ostream& out, const c_stack_frame& frame) {
+	out 	<< "frame[";
+	auto oldwidth = out.width(8);
+	auto oldflags = out.setf(lib::ostream::fmtflags::hex,
+			lib::ostream::fmtflags::basefield);
+	out 	<< const_cast<void *>(static_cast<const void *>(&frame));
+	out 	<< lib::setw(0);
+	out 	<< "]: ebp = ";
+	out 	<< lib::setw(8)
+			<< frame.callers_ebp
+		<< lib::setw(0)
+		<< " ret = "
+		<< lib::setw(8)
+			<< frame.return_address;
+	out.setf(oldflags, lib::ostream::fmtflags::basefield);
+	out.width(oldwidth);
+	return out;
+}
+
+
+bool is_inside_code(void *ptr) {
+	return kobj::code_begin() <= ptr && ptr < kobj::code_end();
+}
+
+
+bool is_inside_stack(void *ptr) {
+	return kobj::stack_begin() <= ptr && ptr < kobj::stack_end();
+}
+
+
 } // namespace
+
 
 void test_backtrace3(int arg __attribute__((unused))) {
 	lib::cout << __func__ << ":" << reinterpret_cast<void *>(test_backtrace3) << lib::endl;
 	execinfo::show_backtrace(nullptr);
 }
+
 
 void execinfo::show_backtrace(void *base_pointer) {
 	if (base_pointer == nullptr) {
@@ -26,20 +60,16 @@ void execinfo::show_backtrace(void *base_pointer) {
 		// adresses right after begging of the function's stack.
 		asm ("mov %%ebp, %%eax\n": "=a"(base_pointer):);
 	}
-	lib::cout << "dumping from stack from " << base_pointer << lib::endl;
+	lib::cout << "dumping from stack from "
+		<< lib::hex << base_pointer << lib::dec << lib::endl;
 
-	auto frame = reinterpret_cast<c_stack_frame *>(base_pointer);
 
-
-	lib::cout << "frame[" << frame << "]: ebp = " << frame->callers_ebp << " ret = " << frame->return_address << lib::endl;
-	frame = reinterpret_cast<decltype(frame)>(frame->callers_ebp);
-	lib::cout << "frame[" << frame << "]: ebp = " << frame->callers_ebp << " ret = " << frame->return_address << lib::endl;
-	frame = reinterpret_cast<decltype(frame)>(frame->callers_ebp);
-	lib::cout << "frame[" << frame << "]: ebp = " << frame->callers_ebp << " ret = " << frame->return_address << lib::endl;
-	frame = reinterpret_cast<decltype(frame)>(frame->callers_ebp);
-	lib::cout << "frame[" << frame << "]: ebp = " << frame->callers_ebp << " ret = " << frame->return_address << lib::endl;
-	frame = reinterpret_cast<decltype(frame)>(frame->callers_ebp);
-	lib::cout << "frame[" << frame << "]: ebp = " << frame->callers_ebp << " ret = " << frame->return_address << lib::endl;
+	auto frame = static_cast<c_stack_frame *>(base_pointer);
+	while (is_inside_stack(frame) &&
+			is_inside_code(frame->return_address)) {
+		lib::cout << *frame << lib::endl;
+		frame = static_cast<decltype(frame)>(frame->callers_ebp);
+	}
 }
 
 namespace {
