bafc4fe72467fa08030c4b8c40f118b184217ec8
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-11-09 00:29:02 +0300

    [testing] generic-lists: add default allocator
    activity: NA

--- testing/generic-lists/generic_list.hpp
+++ testing/generic-lists/generic_list.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
+#include <cstddef>
+
+#include "memory.hpp"
+
 namespace testing {
 
 
-
-
-
-template<class T>
+template<class T, class Alloc = default_allocator<T>>
 class forward_list
 {
 public:
 	using value_type = T;
+	using allocator_type = Alloc;
 	using reference = value_type&;
 	using const_reference = const value_type&;
 	using error_type = bool;
@@ -41,41 +43,43 @@ public:
 private:
 	struct list_item_type;
 	list_item_type* first_ = nullptr;
+	typename allocator_type::template
+		rebind<list_item_type>::other alloc_= {};
 }; // class forward_list
 
 
-template<class T>
-struct forward_list<T>::list_item_type
+template<class T, class Alloc>
+struct forward_list<T, Alloc>::list_item_type
 {
 	T value_;
 	list_item_type* next_;
 };
 
 
-template<class T>
-forward_list<T>::~forward_list()
+template<class T, class Alloc>
+forward_list<T, Alloc>::~forward_list()
 {
 	for (auto it = first_; it != nullptr;) {
 		auto next = it->next_;
-		delete it;
+		alloc_.deallocate(it, 1);
 		it = next;
 	}
 }
 
 
-template<class T>
-void forward_list<T>::pop_front()
+template<class T, class Alloc>
+void forward_list<T, Alloc>::pop_front()
 {
 	auto removed_elem = first_;
 	first_ = first_->next_;
-	delete first_;
+	alloc_.deallocate(first_, 1);
 }
 
 
-template<class T>
-bool forward_list<T>::push_front(const value_type& value)
+template<class T, class Alloc>
+bool forward_list<T, Alloc>::push_front(const value_type& value)
 {
-	list_item_type *elem = new list_item_type();
+	list_item_type *elem = alloc_.allocate(1);
 	if (!elem) {
 		return false;
 	}
