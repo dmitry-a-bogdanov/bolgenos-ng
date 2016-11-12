92ef9a2b80dffebb27593769251ff4d087a97595
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-11-12 20:35:51 +0300

    [testing] generic-lists: use googletest assertions
    activity: NA

--- testing/generic-lists/test_generic_list.cpp
+++ testing/generic-lists/test_generic_list.cpp
@@ -10,36 +10,19 @@ TEST(acts_like_std, add_and_remove_10)
 	std::forward_list<int> s;
 	testing::forward_list<int> t;
 
-	if (s.empty() != t.empty()) {
-		std::clog << "before loop:"
-			<< " s.empty() == " << s.empty()
-			<< " t.empty() == " << t.empty();
-	}
+	ASSERT_EQ(s.empty(), t.empty()) << " before loop";
 
 	for (int i = 0; i < 10; ++i) {
 		s.push_front(i); t.push_front(i);
-		if (s.front() != t.front()) {
-			std::clog << "i: " << i
-				<< " s.front() == " << s.front()
-				<< " t.front() == " << t.front();
-		}
+		ASSERT_EQ(s.front(), t.front()) << " when i = " << i;
 	}
 
 	for(int i = 0; i < 10; ++i) {
-		if (s.empty() != t.empty()) {
-			std::clog << "i: " << i
-				<< " s.empty() == " << s.empty()
-				<< " t.empty() == " << t.empty();
-		}
+		ASSERT_EQ(s.empty(), t.empty()) << " when i = " << i;
 		if (!t.empty()) {
 			s.pop_front(); t.pop_front();
 		}
 	}
 
-	if (s.empty() != t.empty()) {
-		std::clog << "after loop:"
-			<< " s.empty() == " << s.empty()
-			<< " t.empty() == " << t.empty();
-	}
-
+	ASSERT_EQ(s.empty(), t.empty()) << " after loop";
 }
