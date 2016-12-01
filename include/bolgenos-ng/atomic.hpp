a342da43386c1d358a63e0953d604e91d57fc4b0
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2016-12-01 21:25:30 +0300

    atomic: implemented base templated atomics
    activity: #49

--- include/bolgenos-ng/atomic.hpp
+++ include/bolgenos-ng/atomic.hpp
@@ -1,74 +1,249 @@
 #pragma once
 
+#include <lib/type_traits.hpp>
+
+
 namespace atomic {
 
-template<class T>
-struct atomic
+
+template<typename T>
+struct atomic_struct
 {
-public:
-	constexpr atomic(T value) :
+	inline
+	constexpr atomic_struct() = default;
+
+	inline
+	constexpr atomic_struct(T value) :
 		value_(value)
 	{
 	}
 
+	T value_;
+};
 
-	atomic(const atomic& other)
+
+template<typename T, class Enabled = void>
+struct __atomic_base;
+
+
+
+template<typename T>
+struct __atomic_base<T, typename lib::enable_if<(sizeof(T) == 4)>::type> :
+	private atomic_struct<T>
+{
+	using base_type = atomic_struct<T>;
+
+	inline
+	constexpr __atomic_base(T value) :
+		base_type(value)
 	{
-		value_ = other.load();
 	}
 
 
+	inline
 	T load() const
 	{
 		T local;
-		asm("movl %1, %0\n":"=r"(local):"m"(value_));
+		asm("movl %1, %0\n":"=r"(local):"m"(base_type::value_));
 		return local;
 	}
 
 
-	operator T() const
+	inline
+	void store(T value)
+	{
+		asm("movl %1, %0\n":"=m"(base_type::value_):"r"(value));
+	}
+
+
+	inline
+	T fetch_and_add(T increment)
+	{
+		asm("lock; xaddl %k0, %1"
+			: "+r"(increment), "+m"(base_type::value_)
+			:
+			: "memory");
+		return increment;
+	}
+};
+
+
+template<typename T>
+struct __atomic_base<T, typename lib::enable_if<(sizeof(T) == 2)>::type> :
+	private atomic_struct<T>
+{
+	using base_type = atomic_struct<T>;
+
+	inline
+	constexpr __atomic_base(T value) :
+		base_type(value)
+	{
+	}
+
+
+	inline
+	T load() const
+	{
+		T local;
+		asm("movw %1, %w0\n":"=r"(local):"m"(base_type::value_));
+		return local;
+	}
+
+
+	inline
+	void store(T value)
+	{
+		asm("movw %w1, %0\n":"=m"(base_type::value_):"r"(value));
+	}
+
+
+	inline
+	T fetch_and_add(T increment)
+	{
+		asm("lock; xaddw %w0, %1"
+			: "+r"(increment), "+m"(base_type::value_)
+			:
+			: "memory");
+		return increment;
+	}
+};
+
+
+template<typename T>
+struct __atomic_base<T, typename lib::enable_if<(sizeof(T) == 1)>::type> :
+	private atomic_struct<T>
+{
+	using base_type = atomic_struct<T>;
+
+	inline
+	constexpr __atomic_base(T value) :
+		base_type(value)
+	{
+	}
+
+
+	inline
+	T load() const
+	{
+		T local;
+		asm("movb %1, %b0\n":"=r"(local):"m"(base_type::value_));
+		return local;
+	}
+
+
+	inline
+	void store(T value)
+	{
+		asm("movb %b1, %0\n":"=m"(base_type::value_):"r"(value));
+	}
+
+
+	inline
+	T fetch_and_add(T increment)
+	{
+		asm("lock; xaddb %b0, %1"
+			: "+r"(increment), "+m"(base_type::value_)
+			:
+			: "memory");
+		return increment;
+	}
+};
+
+
+template<typename T>
+struct atomic_base :
+	public __atomic_base<T>
+{
+	using specific_base = __atomic_base<T>;
+
+	inline
+	constexpr atomic_base(T value) :
+		specific_base(value)
+	{
+	}
+
+
+	inline
+	T fetch_and_sub(T decrement)
+	{
+		return specific_base::fetch_and_add(-decrement);
+	}
+
+};
+
+
+template<class T>
+struct atomic :
+	private atomic_base<T>
+{
+	using base_type = atomic_base<T>;
+public:
+	atomic() = default;
+
+
+	inline
+	constexpr atomic(T value) :
+		base_type(value)
+	{
+	}
+
+
+	atomic(const atomic& other) = delete;
+
+
+	inline
+	T load() const
+	{
+		return base_type::load();
+	}
+
+
+	inline
+	explicit operator T() const
 	{
 		return load();
 	}
 
 
+	inline
 	void store(T value)
 	{
-		asm("movl %1, %0\n":"=m"(value_):"r"(value));
+		base_type::store(value);
 	}
 
 
-	atomic& operator++()
+	inline
+	T operator++()
 	{
-		asm("lock incl %0\n"::"m"(value_));
-		return *this;
+		T value = base_type::fetch_and_add(static_cast<T>(1));
+		++value;
+		return value;
 	}
 
 
-	atomic operator++(int)
+	inline
+	T operator++(int)
 	{
-		atomic tmp(*this);
-		++(*this);
+		T tmp = base_type::fetch_and_add(static_cast<T>(1));
 		return tmp;
 	}
 
 
-	atomic& operator--()
+	inline
+	T operator--()
 	{
-		asm("lock decl %0\n"::"m"(value_));
-		return *this;
+		T value = base_type::fetch_and_sub(static_cast<T>(1));
+		--value;
+		return value;
 	}
 
 
-	atomic operator--(int)
+	inline
+	T operator--(int)
 	{
-		atomic tmp(*this);
-		--(*this);
+		T tmp = base_type::fetch_and_sub(static_cast<T>(1));
 		return tmp;
 	}
-
-private:
-	T value_;
 };
 
 
--- testing/asm/tests/atomic.cpp
+++ testing/asm/tests/atomic.cpp
@@ -1,37 +1,93 @@
 #include <bolgenos-ng/atomic.hpp>
 
 #include <gtest/gtest.h>
+#include <limits>
 
-TEST(atomic, store_and_load)
+
+template<typename T>
+class atomic_test: public ::testing::Test
 {
-	atomic::atomic<uint32_t> a_v(42);
-	EXPECT_EQ((uint32_t) 42, a_v);
-	a_v.store(13);
-	EXPECT_EQ((uint32_t) 13, a_v.load());
+};
+
+TYPED_TEST_CASE_P(atomic_test);
+
+TYPED_TEST_P(atomic_test, store_and_load)
+{
+	using limits = std::numeric_limits<TypeParam>;
+	const TypeParam first = limits::max() / 2 - 1;
+	const TypeParam second = first / 2;
+
+	atomic::atomic<TypeParam> a_v(first);
+	EXPECT_EQ(first, static_cast<TypeParam>(a_v));
+
+	a_v.store(second);
+	EXPECT_EQ(second, a_v.load());
 }
 
 
-TEST(atomic, increment)
+TYPED_TEST_P(atomic_test, increment)
 {
-	atomic::atomic<uint32_t> a_v(42);
-	EXPECT_EQ((uint32_t) 42, a_v);
-	atomic::atomic<uint32_t> b_v = ++a_v;
-	EXPECT_EQ((uint32_t) 43, a_v);
-	EXPECT_EQ((uint32_t) 43, b_v);
-	atomic::atomic<uint32_t> c_v(a_v++);
-	EXPECT_EQ((uint32_t) 43, c_v);
-	EXPECT_EQ((uint32_t) 44, a_v);
+	using limits = std::numeric_limits<TypeParam>;
+	const TypeParam first = limits::max() / 2 - 1;
+
+	atomic::atomic<TypeParam> a_v(first);
+	EXPECT_EQ(first, static_cast<TypeParam>(a_v));
+	atomic::atomic<TypeParam> b_v(static_cast<TypeParam>(++a_v));
+	EXPECT_EQ(first + 1, static_cast<TypeParam>(a_v));
+	EXPECT_EQ(first + 1, static_cast<TypeParam>(b_v));
+	atomic::atomic<TypeParam> c_v(static_cast<TypeParam>(a_v++));
+	EXPECT_EQ(first + 1, static_cast<TypeParam>(c_v));
+	EXPECT_EQ(first + 2, static_cast<TypeParam>(a_v));
 }
 
 
-TEST(atomic, decrement)
+TYPED_TEST_P(atomic_test, decrement)
 {
-	atomic::atomic<uint32_t> a_v(42);
-	EXPECT_EQ((uint32_t) 42, a_v);
-	atomic::atomic<uint32_t> b_v = --a_v;
-	EXPECT_EQ((uint32_t) 41, a_v);
-	EXPECT_EQ((uint32_t) 41, b_v);
-	atomic::atomic<uint32_t> c_v(a_v--);
-	EXPECT_EQ((uint32_t) 41, c_v);
-	EXPECT_EQ((uint32_t) 40, a_v);
+	using limits = std::numeric_limits<TypeParam>;
+	const TypeParam first = limits::max() / 2 - 1;
+
+	atomic::atomic<TypeParam> a_v(first);
+	EXPECT_EQ(first, static_cast<TypeParam>(a_v));
+
+	atomic::atomic<TypeParam> b_v(static_cast<TypeParam>(--a_v));
+	EXPECT_EQ(first - 1, static_cast<TypeParam>(a_v));
+	EXPECT_EQ(first - 1, static_cast<TypeParam>(b_v));
+	atomic::atomic<TypeParam> c_v(a_v--);
+	EXPECT_EQ(first - 1, static_cast<TypeParam>(c_v));
+	EXPECT_EQ(first - 2, static_cast<TypeParam>(a_v));
 }
+
+
+TYPED_TEST_P(atomic_test, inc_dec_bounds)
+{
+	using limits = std::numeric_limits<TypeParam>;
+
+	atomic::atomic<TypeParam> a_v(limits::max());
+	// it looks like UB for signed type for both C/C++, but we are working
+	// with registers.
+	++a_v;
+	EXPECT_EQ(limits::min(), a_v.load());
+	--a_v;
+	EXPECT_EQ(limits::max(), a_v.load());
+	a_v++;
+	EXPECT_EQ(limits::min(), a_v.load());
+	a_v--;
+	EXPECT_EQ(limits::max(), a_v.load());
+}
+
+
+using atomic_test_types = ::testing::Types<
+	int8_t, uint8_t, char,
+	int16_t, uint16_t,
+	int32_t, uint32_t
+>;
+
+
+REGISTER_TYPED_TEST_CASE_P(atomic_test,
+	store_and_load,
+	increment,
+	decrement,
+	inc_dec_bounds);
+
+INSTANTIATE_TYPED_TEST_CASE_P(, atomic_test, atomic_test_types);
+
