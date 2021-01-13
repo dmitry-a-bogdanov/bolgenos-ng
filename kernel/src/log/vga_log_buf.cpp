9e1528dda019aeec9665d914cb984b4c8cc5cb3c
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2021-01-13 18:57:17 +0300

    colored log levels

--- kernel/src/log/vga_log_buf.cpp
+++ kernel/src/log/vga_log_buf.cpp
@@ -2,6 +2,8 @@
 
 #include <bolgenos-ng/vga_console.hpp>
 
+using namespace vga_console;
+
 lib::log_level_type
 lib::_impl::vga_log_buf::system_log_level = lib::log_level_type::info;
 
@@ -20,8 +22,10 @@ lib::_impl::vga_log_buf::set_system_log_level(log_level_type log_level) {
 
 
 
-lib::_impl::vga_log_buf::vga_log_buf(log_level_type log_level, const char* prefix)
-	: log_level_(log_level), prefix_(prefix), show_header_(true) {
+lib::_impl::vga_log_buf::vga_log_buf(log_level_type log_level, const char* prefix,
+				     color_t color)
+	: log_level_(log_level), prefix_(prefix), show_header_(true),
+	expected_color_{color}, saved_color_{color} {
 }
 
 
@@ -33,11 +37,14 @@ int lib::_impl::vga_log_buf::overflow(int c) {
 	if (log_level_ > system_log_level)
 		return c;
 	if (show_header_) {
+		saved_color_ = vga_console::get_fg();
+		set_fg(expected_color_);
 		vga_console::puts(prefix_);
 		show_header_ = false;
 	}
 	vga_console::putc(c);
 	if (c == '\n') {
+		set_fg(saved_color_);
 		show_header_= true;
 	}
 	return c;
