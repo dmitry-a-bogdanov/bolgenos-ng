c97d080f234bda151a5368a3b1343108dadc1e96
Dmitry Bogdanov <dmitry.a.bogdanov@gmail.com>
2021-01-14 22:53:53 +0300

    fix naming

--- include/bolgenos-ng/ost.hpp
+++ include/bolgenos-ng/ost.hpp
@@ -10,22 +10,22 @@ namespace ost {
 void run();
 
 
-inline void show__() {
+inline void show_() {
 	lib::ccrit << lib::endl;
 }
 
 
 template<typename HeadType, typename ...TailTypes>
-void show__(const HeadType &head, TailTypes ...tail) {
+void show_(const HeadType &head, TailTypes ...tail) {
 	lib::ccrit << head;
-	show__(tail...);
+	show_(tail...);
 }
 
 
 template<typename ...Messages>
 void assert(bool expression, Messages... messages) {
 	if (not expression) {
-		show__(messages...);
+		show_(messages...);
 		panic("OST assertion failed.");
 	}
 }
